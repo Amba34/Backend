1874d7766f5b50867050ccec597ef522191bfa8c
Amba34 <ambadasjoshi34@gmail.com>
2025-06-03 04:27:54 +0530

    chore: update package.json to add jest as a devDependency and remove jest config; modify app.js for code formatting

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import e from "express";
+
 
 const app = express();
+
+
+
 
 app.use(cors(
     {
